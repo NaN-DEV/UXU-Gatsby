6cc59d0f7c37ced1b93d72d630fc866186aee134
NaN <nan@MacBook-Air-NaN.local>
2020-06-18 13:45:52 +0200

    Update

--- src/components/atoms/button/button.js
+++ src/components/atoms/button/button.js
@@ -6,20 +6,14 @@ import theme from "../../../layouts/settings"
 
 // IMPORT SETTINGS COMPONENT
 import ButtonIcon from "./icon/icon"
-import ButtonSumbit from "./submit/submit"
+
 import ButtonClassic from "./classic/classic"
 import ButtonCategory from "./category/category"
 
 // CREATE NEW COMPONENT
 const ButttonComponent = props => {
   const { submit, icon, small, box, category, disabled, onClick } = props
-  if (!submit) {
-    return (
-      <>
-        <ButtonSumbit disabled={disabled} onClick={onClick} {...props} small={small} />
-      </>
-    )
-  } else if (category) {
+  if (category) {
     return (
       <>
         <ButtonCategory disabled={disabled} {...props} small={small} box={box} />

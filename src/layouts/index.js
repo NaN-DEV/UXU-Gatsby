9a3ffa2ce040aa79c3a5999afb6c45c0b7e6ef81
Paweł Niedźwiecki <nan@Pawes-MacBook-Air.local>
2021-03-05 13:32:47 +0100

    update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -74,9 +74,10 @@ const Root = props => {
         <meta name="twitter:creator" content={datoCmsSite.globalSeo.twitterAccount} />
 
         <link
+          rel="preload"
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;0,800;1,400;1,700&display=swap"
-          rel="stylesheet"
-          defer
+          as="style"
+          onload="this.onload=null; this.rel='stylesheet'; document.body.classList.add('fontLoaded')"
         />
       </Helmet>
 
